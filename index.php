df518369f05be08098ee9ce8f26422738a294870
Rs047-el <209739223+Rs047-el@users.noreply.github.com>
2025-11-24 12:12:17 +0900

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $no_header_ouput =[
     'user_logout', 'user_check', 'user_save', 'rst_save', 'rev_save',
 ];
 
-$do = $_GET['do'] ?? 'rst_list';
+$do = $_GET['do'] ?? 'user_login';
 if(in_array($do, $no_header_ouput)){
     include "src/{$do}.php";
 } else {
