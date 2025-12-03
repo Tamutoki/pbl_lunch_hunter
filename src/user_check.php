22d6f4c9f62a688d64e249439fbdb47c14e45604
Rs047-el <209739223+Rs047-el@users.noreply.github.com>
2025-12-03 15:14:32 +0900

    Update user_check.php

--- src/user_check.php
+++ src/user_check.php
@@ -1,4 +1,4 @@
-//ログインチェック
+<!--ログインチェック-->
 <?php
 require_once 'model.php';
 $model = new User();
@@ -6,8 +6,8 @@ $model = new User();
 $user_id = $_POST['user_id'];
 $user_password = $_POST['user_password'];
 $where = "user_id='{$user_id}' AND password='{$user_password}'";
-$user = $model->getDetail($where);
-if($user){ //動作チェック用
+$user = $model->get_userDetail($where);
+if($user){
     $_SESSION['user_id'] = $user['user_id'];
     $_SESSION['user_password'] = $user['password'];
     $_SESSION['usertype_id'] = $user['usertype_id'];
